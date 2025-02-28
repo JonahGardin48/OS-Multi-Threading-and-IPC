a78f26c147a1184e84399f46ff384ba125ea2ce3
Jonah Gardin <jonahgardin@gmail.com>
2025-02-28 15:46:50 -0500

    Report Submission

--- ProjectB/obj/Debug/net8.0/ProjectB.AssemblyInfo.cs
+++ ProjectB/obj/Debug/net8.0/ProjectB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1b9ddeb0fb5e1b3a19c723a776631498b7f868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e589abcdc4c043b95285ced8ab6e5ca50e077c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
